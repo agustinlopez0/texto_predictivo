fafdd61f6fa794e1bbbe708ee79aa17cbf804524
root <root@DESKTOP-R6FR4LA>
2023-12-03 01:11:27 -0300

    makefile

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall
+ARTISTAS = Andres_Calamaro Canciones_para_ninos Diego_Torres Fabiana_Cantilo Fito_Paez Luis_Spinetta
+SALIDAS = Salidas Entradas *.out
+
+all: program test
+
+program: main.c
+	$(CC) $(CFLAGS) -o main.out main.c
+
+test: program
+	@for artista in $(ARTISTAS); do \
+		./main.out $$artista; \
+	done
+
+clean:
+	rm -rf $(SALIDAS)
--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> 
+#include <ctype.h>
 #include <string.h>
 #include <assert.h>
 
 #define BUFFER_SIZE 255
 
-void test_sanitizar_string();
+// Función de prueba para la función sanitizar_string
+void test_sanitizar_string(); 
 
-void agregar_entrada(char nombre_archivo[], char nombre_persona[]);
+// Agrega entrada a un archivo
+void agregar_entrada(char nombre_archivo[], char nombre_persona[]); 
 
-char* sanitizar_string(char linea[]);
+// Función para limpiar y formatear una cadena de texto
+char* sanitizar_string(char linea[]); 
 
 int main(int argc, char *argv[]) {
-    // test_sanitizar_string();
+    test_sanitizar_string();
     if (argc != 2) {
         printf("Uso: %s <nombre_persona>\n", argv[0]);
         return 1;
@@ -23,12 +26,19 @@ int main(int argc, char *argv[]) {
     strcpy(nombre_persona, argv[1]);
 
     char cmd[100];
+
+    // Vacía el archivo si existe, de lo contrario, crea un archivo nuevo
     sprintf(cmd, "truncate -s 0 Entradas/%s.txt", nombre_persona);
     system(cmd);
     
+    // Lista los archivos en la carpeta Textos/<nombre_persona> y guarda la lista en archivo.txt
     sprintf(cmd, "ls Textos/%s > archivo.txt", nombre_persona);
-    system(cmd);
+    if(system(cmd)){
+        printf("Error: no existe la persona\n");
+        return 1;
+    }
 
+    // Abre el archivo que contiene los nombres de los textos
     FILE *nombres_textos = fopen("archivo.txt", "r");
     if (nombres_textos == NULL) {
         printf("Error al abrir el archivo.\n");
@@ -36,21 +46,25 @@ int main(int argc, char *argv[]) {
     }
 
     char nombre_texto[BUFFER_SIZE];
-    while (fgets(nombre_texto, BUFFER_SIZE, nombres_textos) != NULL) {
-        nombre_texto[strlen(nombre_texto) - 1] = '\0';
-        agregar_entrada(nombre_texto, nombre_persona);
+
+    // Lee el nombre de los textos uno por uno desde el archivo
+    while (fgets(nombre_texto, BUFFER_SIZE, nombres_textos)) {
+        nombre_texto[strlen(nombre_texto) - 1] = '\0'; // Elimina el '\n' leído por fgets
+        agregar_entrada(nombre_texto, nombre_persona); // Agrega la entrada al archivo correspondiente
     }
 
     fclose(nombres_textos);
 
+    // Ejecuta un script Python con el nombre de la persona como argumento
     sprintf(cmd, "python3 main.py %s", nombre_persona);
     system(cmd);
    
     return 0;
 }
 
-void agregar_entrada(char nombre_archivo[], char nombre_persona[]){
+void agregar_entrada(char nombre_archivo[], char nombre_persona[]) {
     char ruta_textos[100], ruta_entradas[100];
+
     sprintf(ruta_textos, "Textos/%s/%s", nombre_persona, nombre_archivo);
     sprintf(ruta_entradas, "Entradas/%s.txt", nombre_persona);
 
@@ -61,9 +75,10 @@ void agregar_entrada(char nombre_archivo[], char nombre_persona[]){
     }
 
     char buff[BUFFER_SIZE];
+    // Lee el contenido del archivo de texto y lo limpia usando la función sanitizar_string
     while (fgets(buff, BUFFER_SIZE, archivo_textos) != NULL) {
-        char* aux = sanitizar_string(buff);
-        fputs(aux, archivo_entradas);
+        char* aux = sanitizar_string(buff); 
+        fputs(aux, archivo_entradas); 
         free(aux);
     }
     fclose(archivo_textos); 
@@ -73,35 +88,33 @@ void agregar_entrada(char nombre_archivo[], char nombre_persona[]){
 char* sanitizar_string(char linea[]) {
     char* salida = malloc(sizeof(char) * strlen(linea) + 1);
     int cont = 0;
-    for(int i = 0; i < strlen(linea); i++){
-        if(isalpha(linea[i])){
-            salida[cont++] = tolower(linea[i]);
-        } else if(linea[i] == ' ' || linea[i] == '\n'){
-            if(cont >= 1 && isalpha(salida[cont - 1])){
-                salida[cont++] = ' ';
+    for (int i = 0; i < strlen(linea); i++) {
+        if (isalpha(linea[i])) {
+            salida[cont++] = tolower(linea[i]); 
+        } else if (linea[i] == ' ' || linea[i] == '\n') {
+            if (cont >= 1 && isalpha(salida[cont - 1])) {
+                salida[cont++] = ' '; 
             }
-        } else if(linea[i] == '.'){
-            salida[cont++] = '\n';
+        } else if (linea[i] == '.') {
+            salida[cont++] = '\n'; 
         }
     }
-    salida[cont] = '\0';
+    salida[cont] = '\0'; 
     return salida;
 }
 
 void test_sanitizar_string() {
-    // Casos de prueba
     char cadena_prueba1[] = "esta es una cadena sin caracteres especiales";
     char cadena_prueba2[] = "Este es un texto.\nCon saltos de\nlinea.";
     char cadena_prueba3[] = "";
     char cadena_prueba4[] = "TEXTO CON LETRAS MAYUSCULAS";
     char cadena_prueba5[] = "Este es un texto.\n Con saltos de \n linea.";
 
-    // Ejecución de las pruebas
-    assert(strcmp(sanitizar_string(cadena_prueba1), cadena_prueba1) == 0);
-    assert(strcmp(sanitizar_string(cadena_prueba2), "este es un texto\ncon saltos de linea\n") == 0);
-    assert(strcmp(sanitizar_string(cadena_prueba3), "") == 0);
-    assert(strcmp(sanitizar_string(cadena_prueba4), "texto con letras mayusculas") == 0);
-    assert(strcmp(sanitizar_string(cadena_prueba5), "este es un texto\ncon saltos de linea\n") == 0);
+    assert(!strcmp(sanitizar_string(cadena_prueba1), cadena_prueba1));
+    assert(!strcmp(sanitizar_string(cadena_prueba2), "este es un texto\ncon saltos de linea\n"));
+    assert(!strcmp(sanitizar_string(cadena_prueba3), ""));
+    assert(!strcmp(sanitizar_string(cadena_prueba4), "texto con letras mayusculas"));
+    assert(!strcmp(sanitizar_string(cadena_prueba5), "este es un texto\ncon saltos de linea\n"));
 
     printf("Todas las pruebas han pasado correctamente.\n");
 }
